f19a29024a5b72df1837e4c17d817d0e725b0c9f
augusta <addyauguta@gmail.com>
2022-10-12 11:18:42 +0000

    my main and pratise

---
+++ HelloAbia.java
@@ -0,0 +1,28 @@
+public class HelloAbia {
+    public static void main(String[] args){
+        //test it here
+        /*
+         hello comment
+         */
+        System.out.println("Hello World");
+        testVariables();
+    
+    }
+
+    /*
+     Thi piece of code is to tet variable
+     */
+
+     public static void testVariables(){
+        int sampleInt = 20;
+        double sampleDouble = 0.50;
+        float sampleFloat = 40f;
+        String name = "Augusta Addy";
+
+
+        //casting in java
+        double doubleCasted = sampleInt; //this will do a double
+        //int intCasted = (int) sampleDouble; //thi will return to an int
+
+     }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+
+    class HelloWorld {
+        public static void main(String[] args) {
+            int examScore = 90;
+            double dexamScore = examScore;
+            
+        System.out.println("Hello, World!" + String.valueOf(dexamScore));
+    
+        }
+    }
