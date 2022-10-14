57e355f7415191790120833b06b386f6c1a69c7f
augusta <addyauguta@gmail.com>
2022-10-14 09:26:52 +0000

    learnt values addition

---
+++ operators.java
@@ -0,0 +1,10 @@
+public class operators {
+    public static void main(String[] args) {
+        int valueA = 10;
+        int valueB = 20;
+        valueB = 2;
+
+        System.out.println("Result:"+(valueA * valueB));
+    }
+}
+
